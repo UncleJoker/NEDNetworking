1bb28e6c1e332cfe1d96c2defecaa67d20e53c4c
Joker <zhuangjingen@98du.com>
2021-12-17 15:47:35 +0800

    fix拼接baseurl

--- NEDNetWorking/Core/Classes/Founction/NEDNetworking.m
+++ NEDNetWorking/Core/Classes/Founction/NEDNetworking.m
@@ -64,7 +64,6 @@
         
         NEDNetworkBaseModel *resultModel = [NEDNetworkBaseModel new];
         resultModel.data = responseObject;
-        
         compelete(resultModel);
         
     }];
